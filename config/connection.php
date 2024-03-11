8ecd9e3a201f51eff9e447c852eca1fef0bc41ee
Jorge603 <jorgean9999@gmail.com>
2024-03-10 23:13:48 -0600

    Eliminando codigo innecesario

--- config/connection.php
+++ config/connection.php
@@ -16,10 +16,6 @@ $password = "";
 
 try {
   $conn = new PDO("mysql:host=$host;dbname=$database", $user, $password);
-  // foreach ($conn->query("SHOW DATABASES") as $row) {
-  //   print_r($row);
-  // }
-  // die();
 } catch (PDOException $e) {
   die("PDO Connection Error: " . $e->getMessage());
 }
