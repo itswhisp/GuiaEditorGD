0c6c3693f1069b260ae6c4179cec3ffde41e0b1d
Jorge603 <jorgean9999@gmail.com>
2024-03-12 01:46:54 -0600

    Manejo de errores

--- pagina.php
+++ pagina.php
@@ -6,24 +6,18 @@ require "lib/Parsedown.php";
 $guide = filter_input(INPUT_GET, 'g', FILTER_SANITIZE_STRING);
 
 if(empty($guide)){
+    http_response_code(404);
     header ("Location: index.php");
 }
 else{
-
-try{
 
 $stmt = $conn->prepare("SELECT * FROM secciones WHERE nombre_seccion = ?");
 $stmt->execute([$guide]);
 $seccion = $stmt->fetch();
 
-} catch (Exception $e) {
-    header("HTTP/1.1 404 Not Found");
-    echo "Error 404: Guia no encontrada";
-    $seccion["titulo"] = "Error 404: NOT FOUND";
-    exit();
-}
 
-}
+
+
 ?>
 
 
@@ -33,7 +27,7 @@ $seccion = $stmt->fetch();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Guia del Editor - <?php try { echo $seccion["titulo"]; } catch (Exception $error1){echo "Guia no encontrada";}?></title>
+    <title>Guia del Editor - <?php if (!empty($seccion)) { echo $seccion["titulo"]; } else { echo "Guia no encontrada"; } ?></title>
     <link rel="icon" type="image/x-icon" href="favicon.ico">
     <link rel="stylesheet" href="assets/css/styles.css">
     <link rel="stylesheet" href="assets/css/guia.css">
@@ -52,14 +46,15 @@ $seccion = $stmt->fetch();
     </div>
     <div class="titlebar">
         <img class="back" onclick="goBack()" src="assets/img/left.png">
-        <span id="title"><?php echo $seccion["titulo"] ?></span>
+        <span id="title"><?php if (!empty($seccion)) { echo $seccion["titulo"]; } else { echo "Error 404 - Guia no encontrada"; } ?></span>
     </div>
     <div class="container">
         <div class="navbar unselectable" id="guideNavbar"></div>
         <div class="content" id="guideContent">
         <?php
 
-        try{
+        if (!empty($seccion)){
+
             $url = $seccion["seccion_url"];
             
             $contenido = file_get_contents($url);
@@ -69,13 +64,15 @@ $seccion = $stmt->fetch();
             echo Parsedown::instance()
             ->setBreaksEnabled(true) # enables automatic line breaks
             ->text($contenido); 
-            } catch (Exception $error){
-                header("HTTP/1.1 404 Not Found");
-                echo "Error 404: Guia no encontrada";
-                exit();
-            }
+        }
+        else {
+            http_response_code(404);
+            echo "Error 404 - Guia no encontrada";
+        }
 
-        ?>
+            
+
+    } ?>
 
         </div>
     </div>
