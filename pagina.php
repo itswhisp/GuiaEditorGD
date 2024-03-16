792159f105116b6deccfb7ef1204e3deaf1f277e
Jorge603 <jorgean9999@gmail.com>
2024-03-16 02:20:50 -0600

    Documentacion del codigo

--- pagina.php
+++ pagina.php
@@ -1,3 +1,5 @@
+<!-- CONEXION A LA BASE DE DATOS, CARGA DE LIBRERIAS Y CONSULTA A LA BASE DE DATOS PARA MOSTRAR CONTENIDO -->
+
 <?php
 
     require "config/connection.php";
@@ -5,19 +7,20 @@
 
     $guide = filter_input(INPUT_GET, 'g', FILTER_SANITIZE_STRING);
 
-    if(empty($guide)){ // Condicional: "Si en la URL no se agrega un dato valido"
-        header ("Location: index.php"); // Regresa al usuario a la pagina principal
+    if(empty($guide)){ # Condicional: "Si en la URL no se agrega un dato valido"
+        header ("Location: index.php"); # Regresa al usuario a la pagina principal
     }
     else {
 
-    $stmt = $conn->prepare("SELECT * FROM secciones WHERE nombre_seccion = ?"); //Obtiene el valor $_GET de "g" para realizar una consulta a la DB
-    $stmt->execute([$guide]); // Ejecuta de forma segura la consulta MySQL
-    $seccion = $stmt->fetch(); // Obtiene los datos obtenidos de la consulta y los guarda en $seccion
+    $stmt = $conn->prepare("SELECT * FROM secciones WHERE nombre_seccion = ?"); # Obtiene el valor $_GET de "g" para realizar una consulta a la DB
+    $stmt->execute([$guide]); # Ejecuta de forma segura la consulta MySQL
+    $seccion = $stmt->fetch(); # Obtiene los datos obtenidos de la consulta y los guarda en $seccion
 
     # TO-DO: HACER UN EXPLOITPATCH PARA SANITIZAR AUN MEJOR LAS PETICIONES GET
 
 ?>
 
+<!-- ESTRUCTURA DE LA PAGINA + PHP -->
 
 <!DOCTYPE html>
 <html lang="es">
@@ -25,12 +28,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="theme-color" content="#039dfc">
-    <meta name="twitter:title" content="<?php echo 'Guia del Editor';?>">
-    <meta name="twitter:description" content="<?php if (!empty($seccion)) { echo $seccion['titulo']; } else { echo "Enlace invalido"; } ?>">
-    <meta name="twitter:image" content="https://guia.editorgd.xyz/GuiaEditor.png">
+    <!-- METADATOS DE LA PAGINA  -->
+    <meta name="theme-color" content="#039dfc"> <? # METADATO COLOR (DISCORD) ?>
+    <meta name="twitter:title" content="<?php echo 'Guia del Editor';?>"> <? # METADATO TITULO ?>
+    <meta name="twitter:description" content="<?php if (!empty($seccion)) { echo $seccion['titulo']; } else { echo "Enlace invalido"; } ?>"> <? # METADATO DINAMICO DESCRIPCION ?>
+    <meta name="twitter:image" content="https://guia.editorgd.xyz/GuiaEditor.png"> <? # METADATO IMAGEN ?>
     <meta name="twitter:card" content="summary_large_image">
-    <title><?php if (!empty($seccion)) { echo $seccion["titulo"]; } else { echo "Guia no encontrada"; } ?> - Guia del Editor</title>
+    <title><?php if (!empty($seccion)) { echo $seccion["titulo"]; } else { echo "Guia no encontrada"; } ?> - Guia del Editor</title> <? # TITULO DINAMICO DE LA PAGINA ?>
     <link rel="icon" type="image/x-icon" href="favicon.ico">
     <link rel="stylesheet" href="assets/css/styles.css">
     <link rel="stylesheet" href="assets/css/guia.css">
@@ -58,9 +62,9 @@
             <?php
 
                 /**
-                 * CARGA DEL MENU DE NAVEGACION - AUN SIN TERMINAR
+                 * ALGORITMO DE CARGA DEL MENU DE NAVEGACION - AUN SIN TERMINAR
                  * TO-DO: MEJORAR EL CODIGO Y OPTIMIZAR PARA EVITAR SOBRECARGAS
-                 * EN EL SERVIDOR Y CORREGIR ERRORES, DOCUMENTAR ESTO
+                 * EN EL SERVIDOR Y CORREGIR ERRORES
                  */
 
                 $categories = $conn->query("SELECT * FROM categorias");
@@ -96,7 +100,7 @@
         <div class="content" id="guideContent">
         <?php
 
-        if (!empty($seccion)){ // Condicional: "Si la variable $seccion no está vacia"
+        if (!empty($seccion)){ # Condicional: "Si la variable $seccion no está vacia"
 
             //Inicia la carga del archivo markdown
             $url = $seccion["seccion_url"];
@@ -104,8 +108,8 @@
             $Parsedown = new Parsedown();
 
             echo Parsedown::instance()
-            ->setBreaksEnabled(true) // Activa saltos de linea automaticos
-            ->text($contenido); // Comienza la carga del archivo.md obtenido de la base de datos
+            ->setBreaksEnabled(true) # Activa saltos de linea automaticos
+            ->text($contenido); # Comienza la carga del archivo.md obtenido de la base de datos
         }
         else {
             http_response_code(404);
@@ -113,7 +117,7 @@
             echo "<script>document.getElementById('loading').classList.add('hidden'); document.getElementById('error').classList.remove('hidden');</script>";
         }
 
-    } //fin del else de la condicional del inicio del documento (linea 11) ?>
+    } # Fin del else de la condicional del inicio del documento (linea 11) ?>
         </div>
     </div>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/showdown/2.1.0/showdown.min.js"></script>
